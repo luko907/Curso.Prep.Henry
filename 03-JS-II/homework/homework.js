75468c433d1fb45aa870017146989d5894f36b32
Lucas Koch <luko907@hotmail.com>
2021-05-09 21:47:15 -0300

    finish homework js2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -201,9 +201,9 @@ function tieneTresDigitos(numero) {
   }
 }
 
-function doWhile(numero) {
-  //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
+function doWhile(numero) {tando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
+  //Implementar una función tal que vaya aumen
   //Usar el bucle do ... while.
   let i = 0;
   let count = numero;
