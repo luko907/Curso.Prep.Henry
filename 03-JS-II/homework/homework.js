b6fdda5fb5a8a843368e325e396d09fe74043fb3
Lucas Koch <luko907@hotmail.com>
2021-05-10 18:53:20 -0300

    fix typo homework js2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -201,7 +201,8 @@ function tieneTresDigitos(numero) {
   }
 }
 
-function doWhile(numero) {tando el valor recibido en 5 hasta un límite de 8 veces
+function doWhile(numero) {
+  //tando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Implementar una función tal que vaya aumen
   //Usar el bucle do ... while.
